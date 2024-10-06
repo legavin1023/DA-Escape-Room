082b73d2a9415ba2a404061c574316871817ad7b
huiseon <legavin1023@gmail.com>
2024-10-06 16:41:19 +0900

    페이지 올리기

--- DA_Blight/vite.config.js
+++ DA_Blight/vite.config.js
@@ -1,21 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@import "@/assets/scss/index.scss";`
-  //     }
-  //   }
-  // },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/<DA_Escape_Room>/'  // GitHub Pages의 리포지토리 이름으로 변경
+    : '/',  // 개발 환경에서는 루트 경로
+});
