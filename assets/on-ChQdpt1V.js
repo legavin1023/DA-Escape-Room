30f53df7d94b7b798b209f536f8f6fbbacd2bf56
huiseon <legavin1023@gmail.com>
2024-10-12 16:06:50 +0900

    Updates

---
+++ assets/on-ChQdpt1V.js
@@ -0,0 +1,1 @@
+const o="/DA-Escape-Room/image/off.png",a="/DA-Escape-Room/image/on.png";export{o as a,a as o};
--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const CACHE_NAME = 'v1';
 const urlsToCache = [
-    '/src/assets/image/map1/background_1.png',
-    '/src/assets/image/next_btn.png',
-    '/src/assets/image/clue_ui.png',
-    '/src/assets/image/map1/map_1_1.png',
-    '/src/assets/image/map1/map_1_2_1.png',
-    '/src/assets/image/map1/map_1_2_2.png',
-    '/src/assets/image/map1/map_1_3_1.png',
-    '/src/assets/image/map1/map_1_3_2.png',
-    '/src/assets/image/map2/map_2_1.png',
-    '/src/assets/image/map2/map_2_2_1.png',
-    '/src/assets/image/map2/map_2_2_2.png',
-    '/src/assets/image/map2/map_2_3.png',
-    '/src/assets/image/map2/map_2_4.png',
-    '/src/assets/image/map3/map_3_1.png',
-    '/src/assets/image/map3/map_3_2_1.png',
-    '/src/assets/image/map3/map_3_2_2.png',
-    '/src/assets/image/map3/map_3_3_1.png',
-    '/src/assets/image/map3/map_3_3_2.png'
+    '/image/map1/background_1.png',
+    '/image/next_btn.png',
+    '/image/clue_ui.png',
+    '/image/map1/map_1_1.png',
+    '/image/map1/map_1_2_1.png',
+    '/image/map1/map_1_2_2.png',
+    '/image/map1/map_1_3_1.png',
+    '/image/map1/map_1_3_2.png',
+    '/image/map2/map_2_1.png',
+    '/image/map2/map_2_2_1.png',
+    '/image/map2/map_2_2_2.png',
+    '/image/map2/map_2_3.png',
+    '/image/map2/map_2_4.png',
+    '/image/map3/map_3_1.png',
+    '/image/map3/map_3_2_1.png',
+    '/image/map3/map_3_2_2.png',
+    '/image/map3/map_3_3_1.png',
+    '/image/map3/map_3_3_2.png'
 ];
 
 self.addEventListener('install', event => {
