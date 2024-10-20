35e1ae96170797517f25525f87082f8fe5032abc
huiseon <legavin1023@gmail.com>
2024-10-20 14:12:58 +0900

    스플래시 수정

--- DA_Blight/src/views/splash .vue
+++ DA_Blight/src/views/splash .vue
@@ -2,6 +2,10 @@
   <div>
     <canvas ref="canvas" width="500" height="500"></canvas>
   </div>
+  <audio
+    ref="potetoSound"
+    src="https://legavin1023.github.io/DA-Escape-Room/sound/map_4/빵파레.mp3"
+  ></audio>
 </template>
 
 <script>
@@ -24,6 +28,7 @@ export default {
       gifler('/image/splash/Master_final.gif').get((aGif) => {
         // GIF 애니메이션을 캔버스에 그립니다.
         aGif.animateInCanvas(canvas.value, {
+          loop: false, // 반복하지 않음
           done: () => {
             router.push({ name: 'main' }) // GIF가 끝났을 때 이동할 라우트
           }
@@ -42,3 +47,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+/* 캔버스 크기 조정 */
+canvas {
+  width: 100%;
+  height: auto;
+}
+</style>
